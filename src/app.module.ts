30a4f3aff98a491a81fb3943eeb6734bcd30dc8f
Matheus M <matheus150101miranda@gmail.com>
2024-03-11 21:40:50 -0300

    fix(api): implemented fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriasModule } from './categorias/categorias.module';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { JogadoresModule } from './jogadores/jogadores.module';
     ),
     CategoriasModule,
     JogadoresModule,
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 const logger = new Logger('Main');
+const configService = new ConfigService();
 
 async function bootstrap() {
+  const RABBITMQ_USER = configService.get<string>('RABBITMQ_USER');
+  const RABBITMQ_PASS = configService.get<string>('RABBITMQ_PASS');
+  const RABBITMQ_URL = configService.get<string>('RABBITMQ_URL');
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://admin:123123@localhost:5672/smartranking'],
+        urls: [`amqp://${RABBITMQ_USER}:${RABBITMQ_PASS}@${RABBITMQ_URL}`],
         noAck: false,
         queue: 'admin-backend',
       },
     },
   );
 
-  await app.listen().then(() => logger.log('Microservice is listening'));
+  await app.listen().then(() => logger.log('Micro-admin is listening'));
 }
 bootstrap();
